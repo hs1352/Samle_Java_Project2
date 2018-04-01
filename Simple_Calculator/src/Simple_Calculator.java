2b027eb3532e13c61949bfeb7adf45eb2e825d5d
hs <hs@hs-PC>
2018-04-02 01:53:34 +0900

    집22

--- Simple_Calculator/src/Simple_Calculator.java
+++ Simple_Calculator/src/Simple_Calculator.java
@@ -3,24 +3,7 @@ public class Simple_Calculator {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-<<<<<<< HEAD
-	//학원1
-=======
-	//집
->>>>>>> branch 'master' of https://github.com/hs1352/Samle_Java_Project2.git
-		
-		//학원2
-=======
-	//집1
-	//집2
-<<<<<<< HEAD
-	//집3
-=======
-		
->>>>>>> branch 'master' of https://github.com/hs1352/Samle_Java_Project2.git
->>>>>>> branch 'master' of https://github.com/hs1352/Samle_Java_Project2.git
-//집4
+//집2
 	}
 
 }
